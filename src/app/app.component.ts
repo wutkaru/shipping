f8937ad3d5b9eabf92fdf17ca148d9a678409245
w.karunarat <osacn_wuttipichai.karunarat@krungthai.com>
2024-02-03 00:54:15 +0700

    refactor: format code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,40 +5,41 @@ import dayjs from 'dayjs';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    ReactiveFormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'shipping';
   price = new FormControl(null);
-  summary = signal<null | number>(null);    
+  summary = signal<null | number>(null);
   readonly yen = {
     rate: 0.265,
     ship: 40,
-  }
+  };
   onClick() {
     const price = this.price.value || 0;
-    const pattern = /e/
+    const pattern = /e/;
     if (price < 100 || price.toString().match(pattern)) {
       return;
     }
-    this.summary.set(this.calculate(price))
+    this.summary.set(this.calculate(price));
   }
 
   canChargeExtra() {
     const currentDate = dayjs(new Date()).startOf('date');
-    const calculateDate = dayjs('03/01/2024', "MM/DD/YYYY").startOf('date');
+    const calculateDate = dayjs('03/01/2024', 'MM/DD/YYYY').startOf('date');
     return currentDate.isAfter(calculateDate);
   }
 
   calculate(price: number) {
     let summary = 0;
-    const extra = (this.canChargeExtra() && (price < 2000)) ? 50 : 0;
+    const extra = this.canChargeExtra() && price < 2000 ? 50 : 0;
     const cost = Math.ceil((price * this.yen.rate + this.yen.ship) / 5) * 5;
-    summary = ((Math.ceil((265/215 * cost + Math.ceil(100 * cost / 215 / 10) ) / 5) * 5)) + extra;
+    summary =
+      Math.ceil(((265 / 215) * cost + Math.ceil((100 * cost) / 215 / 10)) / 5) *
+        5 +
+      extra;
     return summary;
   }
 }
